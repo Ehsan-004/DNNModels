3ac0f9afa213498b14f603b238b3047d726a8d00
ehsan-004 <ehsankholusi@gmail.com>
2025-10-17 21:28:02 +0330

    UNet models working proparely

--- UNet/UNet.py
+++ UNet/UNet.py
@@ -6,7 +6,7 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels,
                  norm_layer=nn.BatchNorm2d, 
                  activation=nn.ReLU(inplace=True)):
-        super(DoubleConv, self).__init__()
+        super(DoubleConv, self).__init__()        
 
         layers = []
         # Conv → Norm → Act (first layer)
@@ -49,11 +49,11 @@ class Up(nn.Module):
                  up_mode='upconv', 
                  norm_layer=nn.BatchNorm2d, 
                  activation=nn.ReLU(inplace=False)):
-        """_summary_
+        """up sample the input, concatenates it with the skip connection and computes the convolution
 
         Args:
-            in_channels (int): number of 
-            out_channels (int): _description_
+            in_channels (int): number of channels from down layer
+            out_channels (int): number of output channels
             up_mode (str, optional): _description_. Defaults to 'upconv'.
             norm_layer (_type_, optional): _description_. Defaults to nn.BatchNorm2d.
             activation (_type_, optional): _description_. Defaults to nn.ReLU(inplace=False).
@@ -64,12 +64,12 @@ class Up(nn.Module):
         super(Up, self).__init__()
 
         if up_mode == 'upconv':
-            self.up = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             # devided by 2 because of concatenate
         elif up_mode == 'interp':
             self.up = nn.Sequential(
                 nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-                nn.Conv2d(in_channels, in_channels, kernel_size=1)
+                nn.Conv2d(in_channels, in_channels // 2, kernel_size=1)
             )
         else:
             raise ValueError("up_mode should be 'upconv' or 'interp'")
@@ -79,6 +79,7 @@ class Up(nn.Module):
     def forward(self, x1, x2):
         # x1 = input from previous layer (low-res)
         # x2 = skip connection from encoder (high-res)
+        # x2.shape[1] == x1.shape[1] // 2
         x1 = self.up(x1)
 
         # if spatial sizes are not the same, add padding:
@@ -106,13 +107,27 @@ class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, 
                  depth=4, base_channels=64, 
                  norm_layer=nn.BatchNorm2d, activation=nn.ReLU(inplace=False)):
+        """_summary_
+
+        Args:
+            n_channels (int): number of input channels
+            n_classes (int): number of output channels
+            depth (int, optional): _description_. Defaults to 4.
+            base_channels (int, optional): _description_. Defaults to 64.
+            norm_layer (_type_, optional): _description_. Defaults to nn.BatchNorm2d.
+            activation (_type_, optional): _description_. Defaults to nn.ReLU(inplace=False).
+        """
         super(UNet, self).__init__()
 
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.depth = depth
 
+        # ================
         # Encoder
+        # ================
+        
+        # convert input channels to base channels (e.g: 3 -> 64)
         self.inc = DoubleConv(n_channels, base_channels, norm_layer, activation)
         self.downs = nn.ModuleList()
         channels = base_channels
@@ -120,38 +135,49 @@ class UNet(nn.Module):
             self.downs.append(Down(channels, channels*2, norm_layer, activation))  # spatial // 2, channels * 2
             channels *= 2
 
-        # Bottleneck
-        self.bottleneck = DoubleConv(channels, channels*2, norm_layer, activation)
-
+        # ================
         # Decoder
+        # ================
+        
         self.ups = nn.ModuleList()
         for _ in range(depth):
-            self.ups.append(Up(channels*2, channels//2, norm_layer=norm_layer, activation=activation))
+            self.ups.append(Up(channels, channels//2, norm_layer=norm_layer, activation=activation))
             channels //= 2
 
+        # ================
         # Output
+        # ================
+        
         self.outc = OutConv(base_channels, n_classes)
 
     def forward(self, x):
         x_skip = []
 
+        # ================
         # Encoder
+        # ================
+        
+        # convert channels to base channel
         x1 = self.inc(x)
         x_skip.append(x1)
         x_temp = x1
         for down in self.downs:
             x_temp = down(x_temp)
             x_skip.append(x_temp)
-
-        # Bottleneck
-        x_temp = self.bottleneck(x_temp)
-
+        
+        x_skip.pop()
+        
+        # ================
         # Decoder
+        # ================
+        
         for up in self.ups:
             skip = x_skip.pop()  # picking skip connection from the end of the list
             x_temp = up(x_temp, skip)
 
+        # ================
         # Output
+        # ================
+        
         logits = self.outc(x_temp)
         return logits
-
--- UNet/test.py
+++ UNet/test.py
@@ -26,35 +26,35 @@ def test_down():
 
 
 def test_up():
-    model1 = Up(3, 64, 'upconv')
-    model2 = Up(3, 64, 'interp')
+    model1 = Up(64, 32, 'upconv')
+    model2 = Up(64, 32, 'interp')
     
-    conc1 = torch.randn(1, 64, 200, 200)
-    conc2 = torch.randn(1, 64, 101, 101)
+    conc1 = torch.randn(1, 32, 200, 200)
+    # conc2 = torch.randn(1, 64, 101, 101)
     
     data11 = torch.randn(1, 64, 100, 100)
-    data12 = torch.randn(1, 64, 101, 101)
+    # data12 = torch.randn(1, 64, 101, 101)
     data21 = torch.randn(1, 64, 100, 100)
-    data22 = torch.randn(1, 64, 101, 101)
+    # data22 = torch.randn(1, 64, 101, 101)
     
     out11 = model1(data11, conc1)
-    out12 = model1(data12, conc2)
+    # out12 = model1(data12, conc2)
     out21 = model2(data21, conc1)
-    out22 = model2(data22, conc2)
+    # out22 = model2(data22, conc2)
     
-    assert out11.shape[1] == 64
+    assert out11.shape[1] == 32
     assert out11.shape[2] == 200
     assert out11.shape[3] == 200
     
-    print("shape out12")
-    print(out12.shape)
+    # print("\nshape out12")
+    # print(out12.shape)
     
-    assert out21.shape[1] == 64
+    assert out21.shape[1] == 32
     assert out21.shape[2] == 200
     assert out21.shape[3] == 200
     
-    print("shape out22")
-    print(out22.shape)
+    # print("shape out22")
+    # print(out22.shape)
     
 
 def test_out_conv():
@@ -65,6 +65,23 @@ def test_out_conv():
     assert out.shape[1] == 64
     assert out.shape[2] == 224
     assert out.shape[3] == 224
+
+
+def test_unet():
+    model = UNet(3, 23, 4, 64)
+    data1 = torch.randn(1, 3, 224, 224)
+    data2 = torch.randn(1, 3, 225, 225)
+    out1 = model(data1)
+    out2 = model(data2)
+    
+    assert out1.shape[1] == 23
+    assert out1.shape[2] == 224
+    assert out1.shape[3] == 224
+    
+    assert out2.shape[1] == 23
+    assert out2.shape[2] == 225
+    assert out2.shape[3] == 225
+
     
 # def unet_test():
 if __name__ == "__main__":
@@ -76,25 +93,14 @@ if __name__ == "__main__":
     test_down()
     print(" | done")
     
-    print("[code] up")
-    up = torch.nn.ConvTranspose2d(
-        64,
-        128,
-        kernel_size=2,
-        stride=2
-        )
-    
-    d = torch.randn(1, 64, 200, 200)
-    
-    o = up(d)
-    print("shape after up:")
-    print(o.shape)
-    print("done")
-    
     print("[test] up", end="")
     test_up()
     print(" | done")
     
     print("[test] out conv", end="")
     test_out_conv()
+    print(" | done")
+    
+    print("[test] unet", end="")
+    test_unet()
     print(" | done")
